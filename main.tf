4d39046eb678b0d94a1c4cbf5ee3b48ef7cb7975
Muhammet Arslan <m.arslan@DEAMWS4314-1223.local>
2019-05-03 10:12:03 +0200

    new geass webhook repo

--- main.tf
+++ main.tf
@@ -306,7 +306,7 @@ resource "aws_codepipeline_webhook" "webhook" {
 }
 
 module "github_webhooks" {
-  source               = "git::https://github.com/cloudposse/terraform-github-repository-webhooks.git?ref=tags/0.1.1"
+  source               = "git::https://github.com/geass/terraform-github-repository-webhooks.git?ref=tags/0.1.1"
   enabled              = "${local.enabled && var.webhook_enabled == "true" ? "true" : "false"}"
   github_organization  = "${var.repo_owner}"
   github_repositories  = ["${var.repo_name}"]
